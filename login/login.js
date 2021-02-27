6f2b1e5c3bcc220a3b23e1c5c630c4d773dcc4a3
zc3hd <23397295+zc3hd@users.noreply.github.com>
2021-02-27 10:20:59 +0800

    login:  注册功能完成

--- login/login.js
+++ login/login.js
@@ -1,3 +1,6 @@
+let layer = layui.layer;
+
+
 // ------------------------------------------切换
 // 去注册
 $("#goto-register").on("click", function() {
@@ -9,3 +12,41 @@ $("#goto-login").on("click", function() {
   $("#login").show();
   $("#register").hide();
 });
+
+
+
+
+// -------------------------------------------注册
+// html：form表单！按钮/表单元素 name属性和参数名一致！
+// JS ：收集数据！$.ajax()
+
+$("#register form").on("submit", function(e) {
+  e.preventDefault();
+
+  // 1.收集数据  JQ  layui.form模块
+  let data = $(this).serialize();
+
+
+  // 2.提交数据
+  $.ajax({
+    url: "http://ajax.frontend.itheima.net/api/reguser",
+    type: "POST",
+    data: data,
+    success: function(res) {
+      layer.msg(res.message);
+
+      if (res.status == 0) {
+        // 去登录！
+        $("#goto-login").click();
+
+        // 注册表单重置！
+        $("#register form")[0].reset();
+      }
+
+
+    }
+  })
+
+
+
+})
