71ccdf7a153c4c3faa5bad67722b3a6ab2b88af5
zc3hd <23397295+zc3hd@users.noreply.github.com>
2021-03-01 10:12:50 +0800

    article/cart 编辑：表单赋值及提交表单

--- article/category/category.js
+++ article/category/category.js
@@ -1,4 +1,5 @@
 let layer = layui.layer;
+let form = layui.form;
 // 整个例子：ajax带弹窗班  todolist 
 // 回顾：   todolist面试题，从头敲出来！
 // ******************************************************列表加载
@@ -30,7 +31,7 @@ getList();
 
 // ------------------------------------------------------添加类别
 // 1.准备弹窗内容
-let add_str = `<form class="layui-form add-form" action="" style="margin: 30px; margin-left: 0px;" id="add_form">
+let add_str = `<form class="layui-form add-form" action="" style="margin: 30px; margin-left: 0px;">
                 <div class="layui-form-item">
                   <label class="layui-form-label">类别名称</label>
                   <div class="layui-input-block">
@@ -138,7 +139,7 @@ $("tbody").on("click", ".btnDelete", function(e) {
 // ---------------------------------------------------------编辑
 // 步骤：
 //    1.准备编辑HTML结构的字符串：从新增复制过来！
-let edit_str = `<form class="layui-form edit-form" action="" style="margin: 30px; margin-left: 0px;" id="add_form">
+let edit_str = `<form class="layui-form edit-form" action="" style="margin: 30px; margin-left: 0px;" lay-filter="edit">
                 <div class="layui-form-item">
                   <label class="layui-form-label">类别名称</label>
                   <div class="layui-input-block">
@@ -153,15 +154,16 @@ let edit_str = `<form class="layui-form edit-form" action="" style="margin: 30px
                 </div>
                 <div class="layui-form-item">
                   <div class="layui-input-block">
+                    <input type="hidden" name="Id">
                     <button class="layui-btn" lay-submit lay-filter="formDemo">确认修改</button>
                   </div>
                 </div>
+                
               </form>`;
 
 
 //    2..注册点击：如何注册？事件委托！弹窗代码
 $("tbody").on("click", ".btnEdit", function() {
-
 
   layer.open({
     type: 1,
@@ -171,12 +173,25 @@ $("tbody").on("click", ".btnEdit", function() {
 
     // 当弹窗弹出成功时调用这个函数：
     success: (dom, index) => {
+
+
       // 3.1 表单赋值！
       //       1.form.val(); 准备数据对象：{需要：获取 点击编辑 对应这条数据 }
       //       2.对应这条数据：如何获取；删除的时候，id是怎么拿的？
       //       3.回到渲染地方，设置自定义属性  id  name  别名; (3个)
+      let Id = $(this).attr("_id");
+      let name = $(this).attr("_name");
+      let alias = $(this).attr("_alias");
+      form.val("edit", {
+        name,
+        alias,
+        Id,
+      });
 
-      console.log(this);
+
+
+      // 3.2提交数据：提交修改，知道修改的是哪个数据？根据Id;
+      editSub(index);
 
 
 
@@ -195,3 +210,32 @@ $("tbody").on("click", ".btnEdit", function() {
 //    3.弹窗成功出现：
 //        3.1 表单赋值！
 //        3.2 注册提交事件：单独封装函数！
+function editSub(numb) {
+  $(".edit-form").on("submit", function(e) {
+    e.preventDefault();
+
+    // 1.收集数据
+    let data = $(this).serialize();
+
+    // 2.ajax提交
+    $.ajax({
+      url: "/my/article/updatecate",
+      type: "POST",
+      data: data,
+      success: function(res) {
+        layer.msg(res.message);
+        if (res.status == 0) {
+          // 再次调用列表接口：
+          getList();
+
+          // 关闭弹窗：
+          layer.close(numb);
+        }
+      }
+    })
+
+  });
+}
+
+
+// 箭头函数:内部this指向===>没有自己的this,一定上个作用域this;
