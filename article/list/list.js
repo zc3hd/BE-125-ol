2c5a1b3c59e6f4fd95f49c24a9cf619a0856fd10
zc3hd <23397295+zc3hd@users.noreply.github.com>
2021-03-01 15:16:33 +0800

    article/list 分页基本使用

--- article/list/list.js
+++ article/list/list.js
@@ -1,3 +1,6 @@
+let laypage = layui.laypage;
+
+
 // ***********************************************************列表加载
 
 $.ajax({
@@ -5,11 +8,34 @@ $.ajax({
   data: {
     // 分页数据参数：写死
     pagenum: 1,
-    pagesize: 2,
+    pagesize: 5,
   },
   success: function(res) {
     if (res.status == 0) {
-      console.log(res.data);
+
+      // ************************列表渲染
+      let str = "";
+      res.data.forEach(item => {
+        str += `<tr>
+                <td>${item.title}</td>
+                <td>${item.cate_name}</td>
+                <td>${item.pub_date}</td>
+                <td>${item.state}</td>
+                <td>
+                  <button type="button" class="layui-btn layui-btn-xs btn_edit" _id="${item.Id}">编辑</button>
+                  <button type="button" class="layui-btn layui-btn-danger layui-btn-xs btn_delete" _id="${item.Id}">删除</button>
+                </td>
+              </tr>`;
+      });
+      $("tbody").html(str);
+
+      // ***********************分页器初始化
+      laypage.render({
+        elem: 'page',
+        count: res.total // 数据总数，从服务端得到
+      });
+
+
     }
   }
 })
