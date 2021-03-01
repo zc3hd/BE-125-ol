14db734b67e1082e6e2433cf1fa51d84e83d2c1b
zc3hd <23397295+zc3hd@users.noreply.github.com>
2021-03-01 16:33:09 +0800

    article/list 筛选查询

--- article/list/list.js
+++ article/list/list.js
@@ -1,4 +1,5 @@
 let laypage = layui.laypage;
+let form = layui.form;
 
 
 // 全局变量：任何地方都可以用！
@@ -44,7 +45,7 @@ function list() {
 list();
 
 
-// ************************************************************加载分业器
+// ************************************************************加载分页器
 function pageRender(total) {
   laypage.render({
     elem: 'page',
@@ -57,31 +58,68 @@ function pageRender(total) {
     limits: [2, 3, 5, 10], // 控制分页器，用户选择每页可以显示几条！
     layout: ['prev', 'page', 'next', 'count', 'limit', 'skip'], // 有啥功能
 
-    // 分页器：用户点击页码，直接跳到某个页码;
-    //    分页器第一次加载时候，会执行；
-    //    用户改变页码的时候    会执行
-    jump: function(obj, first) {
-      // first：是第一次加载，还是用户操作！
-      // obj：得到操作后：现在是第几页，选择页显示多少个数据
+    // 分页器：用户操作，直接跳到某个页码;
+
+    // 函数：设置给分页监听用户修改动作，会执行的一个函数；
+    //       非用户操作，也会执行
+    jump: function(obj, key) {
+      // key：非用户操作 true，
+      //      用户操作有个值；undefined;
+      // obj：得到当前分页器数据状态
 
 
       // 当是用户操作的时候，才重新加载列表
-      if (first == undefined) {
+      if (key == undefined) {
         // 业务：列表 按照用户的选择(页码2和显示多少条5)重新加载数据！
         data.pagenum = obj.curr;
         data.pagesize = obj.limit;
         list();
       }
-
-      // 问题：页码初始化后列表数据一致重新加载！
-      // 代码：
-      //     1.页码初始化后列表加载完成后，加载pageRender分页器
-      //     2. 当分页器第一次加载的时候，jump函数也会执行！
-      //     3.jump执行又让列表加载了一次，又算是重新第一次加载！
-      //     4.循环往复
-
     }
 
 
   });
 }
+
+
+
+// ***************************************************************加载分类数据
+$.ajax({
+  url: "/my/article/cates",
+  success: function(res) {
+    if (res.status == 0) {
+      let str = "";
+
+      res.data.forEach(item => {
+        str += `<option value="${item.Id}">${item.name}</option>`
+      });
+
+      $(".cart").html(str);
+
+      // 表单的重新渲染：更新渲染！
+      form.render('select');
+    }
+  }
+});
+
+
+// 分类、状态：多个查询条件重新加载列表：
+$(".search").on("submit", function(e) {
+  e.preventDefault();
+
+
+  // 1.收集数据：类别  和 状态  
+  //    jQ   layui.form.val();
+  // let str = $(this).serialize(); // 字符串
+  // let arr = $(this).serializeArray(); // 数组对象；
+  let obj = form.val("search"); // 对象
+
+
+  // 2.重新设置data查询条件，重新加载列表 list();
+  data.cate_id = obj.cate_id;
+  data.state = obj.state;
+  // 业务上设计：带着分类(其他条件)查询列表，从第一页开始；
+  data.pagenum = 1;
+  list();
+
+})
